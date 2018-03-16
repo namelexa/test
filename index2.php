e5284dd4e8414a363cd1c269b842b30102d3c714
Alex Kisilenko <namelexa@gmail.com>
2018-03-16 12:22:34 +0200

    first commit

---
+++ index2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+		<script>
+			document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
+	<div class="container">
+		<div class="item">text 1</div>
+		<div class="item">text 2</div>
+		<div class="item">text 3</div>
+		<div class="item">text 4</div>
+		<div class="item active">text 5</div>
+
+	</div>
+	</div>
+</body>
+</html>
